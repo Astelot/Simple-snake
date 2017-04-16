a86e369fac4f88fc8b2551b99ba5cd92808e56bc
MatejNedic <matejnedic1@gmail.com>
2017-04-16 22:39:35 +0200

    Collision

---
+++ Assets/CollisionManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Wall")
+        {
+            Application.Quit();
+        }
+        else if (Col.gameObject.tag == "Body")
+        {
+            Application.Quit();
+        }
+    }
+}
